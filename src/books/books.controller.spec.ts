abbca60771fec83b98c448834e2ad15c7aa4d3a0
Isola Pelumi <69679124+philtechphilips@users.noreply.github.com>
2024-02-01 00:25:45 +0100

    test: book controller test re-implemented to cover all functions

--- src/books/books.controller.spec.ts
+++ src/books/books.controller.spec.ts
@@ -2,18 +2,27 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
-import { HttpException, HttpStatus } from '@nestjs/common';
+import { HttpException, HttpStatus, NotFoundException } from '@nestjs/common';
 import { UpdateBookDto } from './dto/update-book.dto';
 
 describe('BooksController', () => {
   let controller: BooksController;
+  let service: BooksService;
 
   const mockBooksService = {
     create: jest.fn(dto => {
       return {
+        id: 1,
         ...dto
       }
-    })
+    }),
+    findAll: jest.fn().mockResolvedValue([
+      { id: 1, title: 'Book 1', author: 'Book Author 1', publisher: 'Publisher 1', year: '2000' },
+      { id: 2, title: 'Book 2', author: 'Book Author 2', publisher: 'Publisher 2', year: '2001' }
+    ]),    
+    findOne: jest.fn(),
+    remove: jest.fn(),
+    update: jest.fn()
   }
 
   beforeEach(async () => {
@@ -23,6 +32,7 @@ describe('BooksController', () => {
     }).overrideProvider(BooksService).useValue(mockBooksService).compile();
 
     controller = module.get<BooksController>(BooksController);
+    service = module.get<BooksService>(BooksService);
   });
 
   it('should be defined', () => {
@@ -32,85 +42,44 @@ describe('BooksController', () => {
   describe("create book", () => {
     it('should create a book successfully', async () => {
       const createdBook: CreateBookDto = { title: "title", author: "Mee", publisher: "Publisher", year: "2003" };
-
-      jest.spyOn(controller, 'create').mockResolvedValue({
-        success: true,
-        data: { id: 6, ...createdBook },
-        message: 'Book created successfully!',
-      });
-
       const result = await controller.create(createdBook);
 
-      expect(result).toEqual({
-        success: true,
-        data: { id: 6, ...createdBook },
-        message: 'Book created successfully!',
+      expect(result.data).toEqual({
+        id: 1,
+        ...createdBook
       });
-
-      expect(controller.create).toHaveBeenCalledWith(createdBook);
     });
 
-    it('should handle error if creation fails', async () => {
-      const createBookDto: CreateBookDto = { title: "title", author: "Mee", publisher: "Publisher", year: "2003" };
+    it('should handle other errors correctly', async () => {
+      const createBookDto = new CreateBookDto();
 
-      jest.spyOn(controller, 'create').mockImplementation(() => {
-        throw new HttpException({ success: false, message: 'Something went wrong!' }, HttpStatus.INTERNAL_SERVER_ERROR);
-      });
+      jest.spyOn(service, 'create').mockRejectedValue({ status: 500 });
 
-      try {
-        await controller.create(createBookDto);
-      } catch (error) {
-        expect(error).toBeInstanceOf(HttpException);
-        expect(error.response).toEqual({ success: false, message: 'Something went wrong!' });
-        expect(error.getStatus()).toBe(HttpStatus.INTERNAL_SERVER_ERROR);
-      }
+      await expect(controller.create(createBookDto)).rejects.toThrowError(
+        new HttpException({ success: false, message: 'Something went wrong!' }, HttpStatus.INTERNAL_SERVER_ERROR)
+      );
     });
 
-    it('should return 400 when there is an empty field', async () => {
-      const createBookDto: CreateBookDto = { title: "", author: "Mee", publisher: "Publisher", year: "2003" };
+    it('should handle 400 error correctly', async () => {
+      const createBookDto = new CreateBookDto();
 
-      jest.spyOn(controller, 'create').mockImplementation(() => {
-        throw new HttpException({
-          message: [
-            "title must be longer than or equal to 3 characters",
-            "the book should have a title"
-          ],
-          error: "Bad Request",
-          statusCode: 400
-        }, HttpStatus.BAD_REQUEST);
-      });
+      jest.spyOn(service, 'create').mockRejectedValue({ status: 400, message: 'Bad request' });
 
-      try {
-        await controller.create(createBookDto);
-      } catch (error) {
-        expect(error).toBeInstanceOf(HttpException);
-        expect(error.response).toEqual({
-          message: [
-            "title must be longer than or equal to 3 characters",
-            "the book should have a title"
-          ],
-          error: "Bad Request",
-          statusCode: 400
-        });
-        expect(error.getStatus()).toBe(HttpStatus.BAD_REQUEST);
-      }
+      await expect(controller.create(createBookDto)).rejects.toThrowError(
+        new HttpException({ success: false, message: 'Bad request' }, HttpStatus.BAD_REQUEST)
+      );
     });
-
 
   })
 
   describe('find all books', () => {
-    it('should return all books successfully', async () => {
+    it('should fetch books successfully', async () => {
       const mockBooks = [
         { id: 1, title: 'Book 1', author: 'Book Author 1', publisher: 'Publisher 1', year: '2000' },
         { id: 2, title: 'Book 2', author: 'Book Author 2', publisher: 'Publisher 2', year: '2001' }
       ];
-
-      jest.spyOn(controller, 'findAll').mockResolvedValue({
-        success: true,
-        data: mockBooks,
-        message: 'Books fetched successfully!',
-      });
+      
+      jest.spyOn(service, 'findAll').mockResolvedValue(mockBooks);
 
       const result = await controller.findAll();
 
@@ -122,32 +91,23 @@ describe('BooksController', () => {
     });
 
 
-    it('should throw an error if something goes wrong', async () => {
-      jest.spyOn(controller, 'findAll').mockImplementation(() => {
-        throw new HttpException({ success: false, message: 'Something went wrong!' }, HttpStatus.INTERNAL_SERVER_ERROR);
-      });
+    it('should handle errors correctly', async () => {
+      // Mock the service method to throw an error
+      jest.spyOn(service, 'findAll').mockRejectedValue(new Error('Some error'));
 
-      try {
-        await controller.findAll();
-      } catch (error) {
-        expect(error).toBeInstanceOf(HttpException);
-        expect(error.message).toBe('Something went wrong!');
-        expect(error.getStatus()).toBe(HttpStatus.INTERNAL_SERVER_ERROR);
-      }
+      await expect(controller.findAll()).rejects.toThrowError(
+        new HttpException({ success: false, message: 'Something went wrong!' }, HttpStatus.INTERNAL_SERVER_ERROR)
+      );
     });
   });
 
   describe('find book by id', () => {
-    it('should return a book successfully if found', async () => {
-      const mockBook = { id: 1, title: 'Book 1', author: 'Author 1', publisher: 'Publisher 1', year: '2000' };
-      jest.spyOn(controller, 'findOne').mockResolvedValue({
-        success: true,
-        data: mockBook,
-        message: 'Book fetched successfully!',
-      });
+    it('should return the book when found', async () => {
+      const mockBook = {id: 1, title: 'Book 1', author: 'Book Author 1', publisher: 'Publisher 1', year: '2000'};
+      
+      jest.spyOn(service, 'findOne').mockResolvedValue(mockBook);
 
-      const result = await controller.findOne('1');
-
+      const result = await controller.findOne("1");
       expect(result).toEqual({
         success: true,
         data: mockBook,
@@ -155,37 +115,28 @@ describe('BooksController', () => {
       });
     });
 
-    it('should throw a NOT_FOUND error if the book is not found', async () => {
-      jest.spyOn(controller, 'findOne').mockResolvedValue(null);
-
-      try {
-        await controller.findOne('1');
-      } catch (error) {
-        expect(error).toBeInstanceOf(HttpException);
-        expect(error.message).toBe('Book not found!');
-        expect(error.getStatus()).toBe(HttpStatus.NOT_FOUND);
-      }
+    it('should throw NotFoundException when book is not found', async () => {
+      jest.spyOn(service, 'findOne').mockRejectedValue({ status: 404 });
+    
+      await expect(controller.findOne("1")).rejects.toThrowError(
+        new HttpException({ success: false, message: 'Book not found!' }, HttpStatus.NOT_FOUND)
+      );
     });
 
-    it('should throw an INTERNAL_SERVER_ERROR if an unexpected error occurs', async () => {
-      jest.spyOn(controller, 'findOne').mockImplementation(() => {
-        throw new HttpException({ success: false, message: 'Something went wrong!' }, HttpStatus.INTERNAL_SERVER_ERROR);
-      });
+    it('should handle errors correctly', async () => {
 
-      try {
-        await controller.findOne('1');
-      } catch (error) {
-        expect(error).toBeInstanceOf(HttpException);
-        expect(error.message).toBe('Something went wrong!');
-        expect(error.getStatus()).toBe(HttpStatus.INTERNAL_SERVER_ERROR);
-      }
+      jest.spyOn(service, 'findOne').mockRejectedValue(new Error('Some error'));
+
+      await expect(controller.findOne("1")).rejects.toThrowError(
+        new HttpException({ success: false, message: 'Something went wrong!' }, HttpStatus.INTERNAL_SERVER_ERROR)
+      );
     });
   })
 
   describe('delete book by id', () => {
-    it('should remove a book successfully', async () => {
-      const mockBook = { id: 1, title: 'Book 1', author: 'Author 1', publisher: 'Publisher 1', year: '2000' };
-      jest.spyOn(controller, 'remove').mockResolvedValue({ success: true, data: mockBook, message: 'Book deleted successfully!' });
+    it('should delete the book successfully', async () => {
+      const mockBook = {id: 1, title: 'Book 1', author: 'Book Author 1', publisher: 'Publisher 1', year: '2000'};
+      jest.spyOn(service, 'remove').mockResolvedValue(mockBook);
 
       const result = await controller.remove('1');
 
@@ -196,43 +147,30 @@ describe('BooksController', () => {
       });
     });
 
-    it('should throw an error if book removal fails', async () => {
-      jest.spyOn(controller, 'remove').mockImplementation(() => {
-        throw new HttpException({ success: false, message: 'Something went wrong!' }, HttpStatus.INTERNAL_SERVER_ERROR);
-      });
+    it('should throw an HttpException when an error occurs', async () => {
+      const errorMessage = 'Some error';
+      jest.spyOn(service, 'remove').mockRejectedValue(new Error(errorMessage));
 
-      try {
-        await controller.remove('1');
-      } catch (error) {
-        expect(error).toBeInstanceOf(HttpException);
-        expect(error.message).toBe('Something went wrong!');
-        expect(error.getStatus()).toBe(HttpStatus.INTERNAL_SERVER_ERROR);
-      }
+      await expect(controller.remove('1')).rejects.toThrowError(
+        new HttpException({ success: false, message: 'Something went wrong!' }, HttpStatus.INTERNAL_SERVER_ERROR)
+      );
     });
 
-    it('should propagate HttpException if thrown by booksService', async () => {
-      const httpException = new HttpException({ success: false, message: 'Book not found!' }, HttpStatus.NOT_FOUND);
-      jest.spyOn(controller, 'remove').mockRejectedValue(httpException);
+    it('should rethrow HttpException when an HttpException occurs', async () => {
+      const httpException = new HttpException({ success: false, message: 'Custom error' }, HttpStatus.BAD_REQUEST);
+      jest.spyOn(service, 'remove').mockRejectedValue(httpException);
 
-      try {
-        await controller.remove('1');
-      } catch (error) {
-        expect(error).toBe(httpException);
-      }
+      await expect(controller.remove('1')).rejects.toThrowError(httpException);
     });
   });
 
   describe('update book by id', () => {
-    it('should update a book successfully', async () => {
-      const updateBookDto = { title: 'Updated Book Title', author: 'Updated Author', publisher: 'Updated Publisher', year: '2022' };
-      const mockBook = { id: 1, ...updateBookDto };
-      jest.spyOn(controller, 'update').mockResolvedValue({
-        success: true,
-        data: mockBook,
-        message: 'Book updated successfully!',
-      });
+    it('should update the book successfully', async () => {
+      const mockUpdateBookDto = new UpdateBookDto();
+      const mockBook = {id: 1, title: 'Book 1', author: 'Book Author 1', publisher: 'Publisher 1', year: '2000'};
+      jest.spyOn(service, 'update').mockResolvedValue(mockBook);
 
-      const result = await controller.update('1', updateBookDto);
+      const result = await controller.update('1', mockUpdateBookDto);
 
       expect(result).toEqual({
         success: true,
@@ -241,31 +179,22 @@ describe('BooksController', () => {
       });
     });
 
-    it('should throw an error if book update fails', async () => {
-      const updateBookDto = { title: 'Updated Book Title', author: 'Updated Author', publisher: 'Updated Publisher', year: '2022' };
-      jest.spyOn(controller, 'update').mockImplementation(() => {
-        throw new HttpException({ success: false, message: 'Something went wrong!' }, HttpStatus.INTERNAL_SERVER_ERROR);
-      });
+    it('should throw an HttpException when an error occurs', async () => {
+      const errorMessage = 'Some error';
+      const mockUpdateBookDto = new UpdateBookDto();
+      jest.spyOn(service, 'update').mockRejectedValue(new Error(errorMessage));
 
-      try {
-        await controller.update('1', updateBookDto);
-      } catch (error) {
-        expect(error).toBeInstanceOf(HttpException);
-        expect(error.message).toBe('Something went wrong!');
-        expect(error.getStatus()).toBe(HttpStatus.INTERNAL_SERVER_ERROR);
-      }
+      await expect(controller.update('1', mockUpdateBookDto)).rejects.toThrowError(
+        new HttpException({ success: false, message: 'Something went wrong!' }, HttpStatus.INTERNAL_SERVER_ERROR)
+      );
     });
 
-    it('should propagate HttpException if thrown by booksService', async () => {
-      const updateBookDto = { title: 'Updated Book Title', author: 'Updated Author', publisher: 'Updated Publisher', year: '2022' };
-      const httpException = new HttpException({ success: false, message: 'Book not found!' }, HttpStatus.NOT_FOUND);
-      jest.spyOn(controller, 'update').mockRejectedValue(httpException);
+    it('should rethrow HttpException when an HttpException occurs', async () => {
+      const httpException = new HttpException({ success: false, message: 'Custom error' }, HttpStatus.BAD_REQUEST);
+      const mockUpdateBookDto = new UpdateBookDto();
+      jest.spyOn(service, 'update').mockRejectedValue(httpException);
 
-      try {
-        await controller.update('1', updateBookDto);
-      } catch (error) {
-        expect(error).toBe(httpException);
-      }
+      await expect(controller.update('1', mockUpdateBookDto)).rejects.toThrowError(httpException);
     });
   });
 });
